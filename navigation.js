964452f1ca9a83afbb5a5b03936657cd9430990f
Farid Amimri <farid.amimri@gmail.com>
2023-08-09 23:05:39 +0200

    Cart Screen UI

--- navigation.js
+++ navigation.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './screens/HomeScreen'
 import RestaurantScreen from './screens/RestaurantScreen'
+import CartScreen from './screens/CartScreen'
+import OrderPrepairingScreen from './screens/OrderPrepairingScreen'
 
 export default function Navigation() {
   const Stack = createNativeStackNavigator()
@@ -13,6 +15,16 @@ export default function Navigation() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='Restaurant' component={RestaurantScreen} />
+        <Stack.Screen
+          name='Cart'
+          component={CartScreen}
+          options={{ presentation: 'modal' }}
+        />
+        <Stack.Screen
+          name='OrderPrepairing'
+          component={OrderPrepairingScreen}
+          options={{ presentation: 'fullScreenModal' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
