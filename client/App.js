27f250eb4a2c9cd139b9f864397a3bde3d17f9f8
Farid Amimri <farid.amimri@gmail.com>
2023-08-17 12:11:51 +0200

    Move files to client folder

---
+++ client/App.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import Navigation from './navigation'
+import { store } from './store'
+import { Provider } from 'react-redux'
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  )
+}
---
+++ client/store.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import { configureStore } from '@reduxjs/toolkit'
+import cartSlice from './redux/cartSlice'
+import restaurantSlice from './redux/restaurantSlice'
+
+export const store = configureStore({
+  reducer: {
+    cart: cartSlice,
+    restaurant: restaurantSlice
+  }
+})
